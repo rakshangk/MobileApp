3858e92402f772ded5c8769a0a46476f194cb3e8
Arsahd <arshad@techmust.com>
2018-09-05 15:32:18 +0530

    Added a  new tenant_list file --Arshad

---
+++ login_flutter/lib/models/tenant_list.dart
@@ -0,0 +1,24 @@
+class TenantList {
+  String m_strUsername;
+  String m_strPassword;
+  List<String> arr_tenantList;
+
+  TenantList(this.m_strUsername, this.m_strPassword, this.arr_tenantList);
+  String get username => m_strUsername;
+  String get password => m_strPassword;
+  List get arrJobList => arr_tenantList;
+
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
+    map["username"] = m_strUsername;
+    map["password"] = m_strPassword;
+    map["arrJobList"] = arr_tenantList;
+    return map;
+  }
+
+  TenantList.map(dynamic obj) {
+    this.arr_tenantList = obj["arrJobList"];
+    this.m_strUsername = obj["username"];
+    this.m_strPassword = obj["password"];
+  }
+}
