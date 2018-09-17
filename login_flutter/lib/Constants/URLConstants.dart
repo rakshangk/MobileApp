8dcb54970eb1a9f5c88b4f580cf845adc4d03ecf
Arsahd <arshad@techmust.com>
2018-09-17 17:33:43 +0530

    Modified for comment line --Arshad

--- login_flutter/lib/Constants/URLConstants.dart
+++ login_flutter/lib/Constants/URLConstants.dart
@@ -1,6 +1,6 @@
 class URLConstants {
-   static String strBaseURL = //"http://192.168.1.3:8080/techmust"; //LOCAL CONNECTION
-      "http://authframework-eapp.mgtmzrenwf.ap-southeast-1.elasticbeanstalk.com"; //AWS CONNECTION
+   static String strBaseURL = //"http://192.168.1.3:8080/techmust"; //LOCAL CONNECTION String
+      "http://authframework-eapp.mgtmzrenwf.ap-southeast-1.elasticbeanstalk.com"; //AWS CONNECTION String
    String strLoginURL = strBaseURL + "/signIn";
    String strTenantURL = strBaseURL + "/listJobs";
    String strLogoutURL = strBaseURL + "/logout";
