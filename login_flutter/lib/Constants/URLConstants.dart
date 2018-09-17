d3e83fd50e376fc9d127500f1ba7c223df75a961
Arsahd <arshad@techmust.com>
2018-09-17 16:59:57 +0530

    MODIFIED THE URL constants --Arshad

--- login_flutter/lib/Constants/URLConstants.dart
+++ login_flutter/lib/Constants/URLConstants.dart
@@ -1,6 +1,6 @@
 class URLConstants {
    static String strBaseURL = //"http://192.168.1.3:8080/techmust"; //LOCAL CONNECTION
-      "http://authframework-eapp.mgtmzrenwf.ap-southeast-1.elasticbeanstalk.com";
+      "http://authframework-eapp.mgtmzrenwf.ap-southeast-1.elasticbeanstalk.com"; //AWS CONNECTION
    String strLoginURL = strBaseURL + "/signIn";
    String strTenantURL = strBaseURL + "/listJobs";
    String strLogoutURL = strBaseURL + "/logout";
